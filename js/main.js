3beb06d94ca2d13e685ac0a93609173b6cb9b833
narspi <40424457+narspi@users.noreply.github.com>
2020-08-16 18:22:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
-let mySwiper = new Swiper('.swiper-container', {
-    loop: true,
-    slidesPerView: 4,
-  })
+const   mainForm = document.querySelector('.main__form'),
+        closeBtn = document.querySelector('.close-btn'),
+        mainVideoBtn = document.querySelector('.main__video-btn'),
+        modal = document.querySelector('.modal'),
+        header = document.querySelector('.header'),
+        main = document.querySelector('.main');
 
-setTimeout(()=> {
-  let next = document.querySelector('.swiper-slide-next');
-  let accent = next.nextElementSibling;
-  accent.classList.add('swiper-slide--accent');
-},1000);
-  
 
-mySwiper.on('slideChange', ()=> {
-  setTimeout(()=> {
-    let active = document.querySelector('.swiper-slide-active');
-    next = document.querySelector('.swiper-slide-next');
-    accent = next.nextElementSibling;
-    let back = accent.nextElementSibling;
-    accent.classList.add('swiper-slide--accent');
-    active.classList.remove('swiper-slide--accent');
-    next.classList.remove('swiper-slide--accent');
-    back.classList.remove('swiper-slide--accent');
-  },100);
+
+
+// btn.addEventListener('click', ()=> {
+//     form.innerHTML = `<input type="text" placeholder="input somethink">`;
+// });
+
+const  formAnima = function (event) {
+    mainForm.classList.remove('main__form--accent');
+    setTimeout(()=> {
+        if (!(event.target.closest('.main__form'))) 
+        mainForm.classList.add('main__form--accent');
+    }, 100);
+}
+
+
+document.addEventListener('click', formAnima);
+
+mainVideoBtn.addEventListener('click', ()=> {
+    modal.classList.add('modal--active');
+    header.classList.add('blur');
+    main.classList.add('blur');
+    document.removeEventListener('click', formAnima);
+});
+
+closeBtn.addEventListener('click', ()=> {
+    modal.classList.remove('modal--active');
+    header.classList.remove('blur');
+    main.classList.remove('blur');
+    setTimeout(function () {
+        document.addEventListener('click', formAnima);
+    },50);
 });
